f0d8b2b976b3ece73ae0bd95bde04dc115021dd4
2hm1901 <2hm1901dev@gmail.com>
2024-07-11 22:09:52 +0700

    Create Event Bus

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -4,12 +4,14 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
+import { useEventBus } from '@/EventBus';
 
 export default function Authenticated({ header, children }) {
     const page = usePage();
     const user = page.props.auth.user;
     const conversations = page.props.conversations;
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+    const {emit} = useEventBus();
 
     useEffect(() => {
         conversations.forEach((conversation) => {
@@ -32,37 +34,39 @@ export default function Authenticated({ header, children }) {
                     console.log("SocketMessage", e);
                     const message = e.message;
 
-                    // emit("message.created", message);
+                    emit("message.created", message);
                     if(message.sender_id === user.id){
                         return;
                     }
-                    // emit("newMessageNotification", {
-                    //     user: message.sender,
-                    //     group_id: message.group_id,
-                    //     message: 
-                    //         message.message ||
-                    //         `Shared ${
-                    //             message.attachments.length === 1
-                    //                 ? "an attachment"
-                    //                 : message.attachments.length + 
-                    //                     " attachments"
-                    //         }`,
-                    // });
+                    emit("newMessageNotification", {
+                        user: message.sender,
+                        group_id: message.group_id,
+                        message: 
+                            message.message ||
+                            `Shared ${
+                                message.attachments.length === 1
+                                    ? "an attachment"
+                                    : message.attachments.length + 
+                                        " attachments"
+                            }`,
+                    });
                 });
         });
 
         return () => {
-            let channel = `message.group.${conversation.id}`;
+            conversations.forEach((conversation) => {
+                let channel = `message.group.${conversation.id}`;
 
-            if(conversation.is_user){
-                channel = `message.user.${[
-                    parseInt(user.id),
-                    parseInt(conversation.id),
-                ]
-                    .sort((a, b) => a - b)
-                    .join("-")}`;
-            }
-            Echo.leave(channel);
+                if(conversation.is_user){
+                    channel = `message.user.${[
+                        parseInt(user.id),
+                        parseInt(conversation.id),
+                    ]
+                        .sort((a, b) => a - b)
+                        .join("-")}`;
+                }
+                Echo.leave(channel);
+            });
         };
     }, [conversations]);
     return (
