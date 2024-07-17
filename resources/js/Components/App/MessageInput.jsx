51377bb0c344f9ad232a1b4fe2f5f70cf8835f65
2hm1901 <2hm1901dev@gmail.com>
2024-07-17 20:55:48 +0700

    Rercord and send audio

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -14,6 +14,7 @@ import axios from "axios";
 import { isAudio, isImage } from "@/helpers";
 import CustomAudioPlayer from "./CustomAudioPlayer";
 import AttachmentPreview from "./AttachmentPreview";
+import AudioRecorder from "./AudioRecorder";
 
 
 const MessageInput = ({conversation = null}) => {
@@ -101,7 +102,12 @@ const MessageInput = ({conversation = null}) => {
 
         axios
             .post(route("message.store"), data)
+    };
+
+    const recordedAudioReady = (file, url) => {
+        setChosenFiles((prevFiles) => [...prevFiles,{file,url,}]);
     }
+
     return (
         <div className="flex flex-wrap items-start border-t border-e-slate-700 py-3">
             <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
@@ -124,6 +130,7 @@ const MessageInput = ({conversation = null}) => {
                         className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
                     />
                 </button>
+                <AudioRecorder fileReady={recordedAudioReady}/>
             </div>
             <div className="order-1 px-3 xs:p-0 min-w-[200px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
                 <div className="flex">
